604e96b347b54b8b7aee10b5d09694787a7ced9c
hwanhee <hwanhee@hwanheeui-MacBookPro.local>
2025-03-13 01:41:00 +0900

    Initial commit

---
+++ src/main/java/study/spring_board_V2/SpringBoardV2Application.java
@@ -0,0 +1,13 @@
+package study.spring_board_V2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoardV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoardV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/study/spring_board_V2/SpringBoardV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring_board_V2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoardV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
