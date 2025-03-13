6dca3e962690150c83b92f9625c3b57bee8901d8
hwanhee <hwanhee@hwanheeui-MacBookPro.local>
2025-03-13 17:35:41 +0900

    멤버 서비스 구현 및 테스트 구현

---
+++ src/main/java/study/spring_board_V2/service/MemberService.java
@@ -0,0 +1,48 @@
+package study.spring_board_V2.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import study.spring_board_V2.domain.Member;
+import study.spring_board_V2.repository.MemberRepository;
+
+import java.util.List;
+
+@Service
+public class MemberService {
+    MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Transactional
+    public Long join(Member member) {
+        // 이름 중복 체크
+        Member findMember = memberRepository.findByName(member.getName());
+        if (findMember != null) {
+            throw new IllegalStateException("이미 존재하는 이름입니다.");
+        }
+        // 회원 저장
+        memberRepository.Save(member);
+        return member.getId();
+    }
+
+    public Member signin(String name,String password){
+        Member member = memberRepository.findByName(name);
+        if(member==null || !member.getPassword().equals(password)){
+            throw new IllegalStateException("이름 또는 비밀번호가 잘못되었습니다.");
+        }
+        return member;
+    }
+
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(long id){
+        return memberRepository.findById(id);
+    }
+
+}
