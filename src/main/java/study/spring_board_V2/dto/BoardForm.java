4f2ae46162d6a4f9a97b00903f147b9836590a1c
hwanhee <hwanhee@hwanheeui-MacBookPro.local>
2025-03-14 17:55:06 +0900

    board dto 구현

---
+++ src/main/java/study/spring_board_V2/dto/BoardForm.java
@@ -0,0 +1,11 @@
+package study.spring_board_V2.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BoardForm {
+    private String title;
+    private String content;
+}
