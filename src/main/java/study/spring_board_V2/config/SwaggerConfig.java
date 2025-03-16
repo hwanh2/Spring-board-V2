b80c27178106b0becaf5d2ccdb5950245eda9d29
hwanhee <hwanhee@hwanheeui-MacBookPro.local>
2025-03-16 12:47:56 +0900

    스웨거 설정 수정

--- src/main/java/study/spring_board_V2/config/SwaggerConfig.java
+++ src/main/java/study/spring_board_V2/config/SwaggerConfig.java
@@ -1,5 +1,8 @@
 package study.spring_board_V2.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,5 +16,15 @@ public class SwaggerConfig {
                 .group("all") // 전체 API 그룹
                 .pathsToMatch("/**") // 모든 API 포함
                 .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Spring Board API 문서")
+                        .version("v2.0")
+                        .description("Spring Boot 기반 게시판 API 문서입니다.")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
     }
 }
